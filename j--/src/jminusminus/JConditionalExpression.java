3e1a35b23ded4e1c22bb2dce35860215fe90707d
Danley Nemorin <danleynemo1995@gmail.com>
2021-04-25 02:10:46 -0400

    WTF

--- j--/src/jminusminus/JConditionalExpression.java
+++ j--/src/jminusminus/JConditionalExpression.java
@@ -37,7 +37,13 @@ class JConditionalExpression extends JExpression {
      * {@inheritDoc}
      */
     public JExpression analyze(Context context) {
-        // TODO
+        condition = (JExpression) condition.analyze(context);
+        condition.type().mustMatchExpected(line(), Type.BOOLEAN);
+        thenPart = (JExpression) thenPart.analyze(context);
+        elsePart = (JExpression) thenPart.analyze(context);
+        //condition.type().mustMatchExpected(line(), Type.BOOLEAN);
+        elsePart.type().mustMatchExpected(line(), thenPart.type());
+        type = thenPart.type();
         return this;
     }
 
@@ -45,7 +51,17 @@ class JConditionalExpression extends JExpression {
      * {@inheritDoc}
      */
     public void codegen(CLEmitter output) {
-        // TODO
+        String elseLabel = output.createLabel();
+        String endLabel = output.createLabel();
+        condition.codegen(output, elseLabel, false);
+        thenPart.codegen(output);
+
+        output.addBranchInstruction(GOTO, endLabel);
+
+        output.addLabel(elseLabel);
+        elsePart.codegen(output);
+        output.addLabel(endLabel);
+
     }
 
     /**
